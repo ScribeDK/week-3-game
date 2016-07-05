6e04b93ec2f24bc06a88f9cd5f1201ea6c166570
John Grant Bunner <scribe@exposed2tce.com>
2016-07-04 20:27:03 -0400

    Hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,46 @@
 	var List = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-	var wordList = ["SUPERWORDTEST"];
+	var wordList = ["GRIFFIN","PHOENIX","SPHINX","MANTICORE","WYVERN","TRITON","DRAKE","ROC"];
 	var wordLength = [];
-	var wordChosen = wordList[Math.floor(Math.random() * wordList.length)];
 	var outList = [];
+	var guessCount = 1;
+	var letterCount = 1;
+	var newGame = true;
+	var wordChosen = ("");
+	var letterCheck = [""];
+	var winWord = [];
+	var indexNumber = 0;
 
 	
-	function nameMaker (x){
-		var y = x.toUpperCase().charAt(0);
-		for(i in x){
-		if (i > 0){
-		y = y + x.toLowerCase().charAt(i);
-		}}
-		console.log(y);
-		}
+	function startGame(){
+	
+	indexNumber = Math.floor(Math.random() * wordList.length);
+	wordChosen = wordList[indexNumber];
 	
 	for (i in wordChosen){
 		wordLength.push("<span class='answer'>_</span>");
+		winWord.push("_");
 	}
+	
+	for(x in wordChosen){
+		var check = false;
+		for(t in letterCheck){
+		if (wordChosen[x] == letterCheck[t]){
+			var check = true;
+		}}
+		if(check == false){
+		letterCheck.push(wordChosen[x])
+		}
+		
+	letterCount = letterCheck.length;
+	
+	guessCount = 1 + letterCount/2;
+	
+	for(e in List){
+	outList[e] = ("<span class='boxLetter'>" + List[e] + "</span>");}
+	
+	newGame = false;
+	}}
+
 	
 	function outputAns(){
 		var Answer = ("");
@@ -24,11 +48,9 @@
 		{
 			Answer = Answer + wordLength[a];
 		}
-	document.querySelector('.Wordbox').innerHTML = ("<br><br><br>" + Answer);}	
+	document.querySelector('.Wordbox').innerHTML = ("<br><br><br>" + Answer + "<br> Remaining Guesses: " + guessCount);}	
 	
-	for(e in List){
-	outList[e] = ("<span class='boxLetter'>" + List[e] + "</span>");} 
-	
+	 	
 	function output(){
 		var x = 1;
 		var write = ("");
@@ -46,25 +68,61 @@
 	document.querySelector('.Letters').innerHTML = ("<br>" + write);
 	}
 	
+	
+	startGame();
 	output();
 	outputAns();
 	
 	document.onkeyup = function (event) {
-	
+		if(guessCount > 0){
 		var userInput = String.fromCharCode(event.keyCode).toUpperCase();
 		var wordCheck = false;
-		for(x in wordChosen)
+		for(x in wordChosen){
 		if(userInput == wordChosen[x]){
 		wordLength[x] = ("<span class='answer'>"+ wordChosen[x] +"</span>");
+		winWord[x] = (userInput);
+		console.log(userInput);
+		console.log(winWord);
 		var wordCheck = true;
-		}
+		}}
 		for(e in List){		
 			if(userInput == List[e]){
 				if (wordCheck == false){
-					outList[e] = ("<span class='boxLetter negLetter'>" + userInput + "</span>");}
+					outList[e] = ("<span class='boxLetter negLetter'>" + userInput + "</span>");
+					guessCount = guessCount - 1;}
 				else{outList[e] = ("<span class='boxLetter posLetter'>" + userInput + "</span>");}
 			}
+		}}
+		else{guessCount = -1;}
+	
+	if(guessCount < 0){
+		newGame = confirm('Play again?');
+		guessCount = 0;
+	}
+
+	var win = false;
+	var winString = (wordList[indexNumber]);
+	for(e in winString){
+		if (winString[e] == winWord[e])
+		{win = true;}
+		else{win = false;
+			break;}
+	}
+	
+
+	console.log(win);
+	
+	if (win == true)
+		{guessCount = 0;
+		alert('You win press anykey to play another game.');
+		winWord = [];
 		}
+	
+	if (newGame == true)
+	{wordLength = [];
+	winWord = [];
+	startGame();
+	}
 	
 	output();
 	outputAns();
