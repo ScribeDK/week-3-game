ddc909de059daca1a7af647ed56c1610bff70aa5
John Grant Bunner <scribe@exposed2tce.com>
2016-07-05 14:32:29 -0400

    Hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
 	var List = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+	var winPicture = ["<img width='436' height='550' alt='Bestiary_Griffin' src='assets/images/Bestiary_Griffin.jpg'/>","<img width='736' height='492' alt='Bestiary_Phoenix' src='assets/images/Bestiary_Phoenix.jpg'/>","<img width='436' height='550' alt='Bestiary_Sphinx' src='assets/images/Bestiary_Sphinx.jpg'/>","<img width='399' height='550' alt='Bestiary_Manticore' src='assets/images/Bestiary_Manticore.jpg'/>","<img width='800' height='533' alt='Bestiary_Wyvern' src='assets/images/Bestiary_Wyvern.jpg'/>","<img width='436' height='550' alt='Bestiary_Triton' src='assets/images/Bestiary_Triton.jpg'/>","<img width='436' height='550' alt='Bestiary_Firedrake' src='assets/images/Bestiary_Firedrake.jpg'/>","<img width='436' height='550' alt='Bestiary_Roc' src='assets/images/Bestiary_Roc.jpg'/>"];
 	var wordList = ["GRIFFIN","PHOENIX","SPHINX","MANTICORE","WYVERN","TRITON","DRAKE","ROC"];
+	var hangPictures = ["<img width='978' height='550' alt='HangMan0' src='assets/images/HangMan0.jpg'/>","<img width='978' height='550' alt='HangMan1' src='assets/images/HangMan1.jpg'/>","<img width='978' height='550' alt='HangMan2' src='assets/images/HangMan2.jpg'/>","<img width='978' height='550' alt='HangMan3' src='assets/images/HangMan3.jpg'/>","<img width='978' height='550' alt='HangMan4' src='assets/images/HangMan4.jpg'/>","<img width='978' height='550' alt='HangMan5' src='assets/images/HangMan5.jpg'/>","<img width='978' height='550' alt='HangMan6' src='assets/images/HangMan6.jpg'/>","<img width='978' height='550' alt='HangMan7' src='assets/images/HangMan7.jpg'/>"];
 	var wordLength = [];
 	var outList = [];
 	var guessCount = 1;
@@ -9,7 +11,8 @@
 	var letterCheck = [""];
 	var winWord = [];
 	var indexNumber = 0;
-
+	var startGuess = 0;
+	var lost = false;
 	
 	function startGame(){
 	
@@ -33,10 +36,13 @@
 		
 	letterCount = letterCheck.length;
 	
-	guessCount = 1 + letterCount/2;
+	guessCount = Math.ceil(1 + letterCount/2);
+	startGuess = guessCount;
+	lost = false;
 	
 	for(e in List){
 	outList[e] = ("<span class='boxLetter'>" + List[e] + "</span>");}
+	document.querySelector('.HangMan').innerHTML = ("<br>" + hangPictures[0]);
 	
 	newGame = false;
 	}}
@@ -100,6 +106,22 @@
 		guessCount = 0;
 	}
 
+	
+	if(startGuess - guessCount == 0){
+		document.querySelector('.HangMan').innerHTML = ("<br>" + hangPictures[0]);
+	}
+	else if(guessCount == 1){
+		document.querySelector('.HangMan').innerHTML = ("<br>" + hangPictures[6]);
+		lost = true;
+	}
+	else if(lost == true && guessCount == 0){
+		document.querySelector('.HangMan').innerHTML = ("<br>" + hangPictures[7]);
+		alert('You lost press anykey to play another game.');
+		lost = false	
+	}
+	
+	
+	
 	var win = false;
 	var winString = (wordList[indexNumber]);
 	for(e in winString){
@@ -114,6 +136,7 @@
 	
 	if (win == true)
 		{guessCount = 0;
+		document.querySelector('.HangMan').innerHTML = ("<br>" + winPicture[indexNumber]);
 		alert('You win press anykey to play another game.');
 		winWord = [];
 		}
